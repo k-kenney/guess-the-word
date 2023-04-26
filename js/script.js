00c4fdd230266a204cf4a3b2d2a3841d040ff3a3
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-04-26 10:35:02 +0200

    created variables for project

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const guessButton = document.questSelectoy(".guess");
+const inputLetter = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remaining = document.querySelector(".remaining");
+const remainingSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const playAgain = document.querySelector(".play-again");
+
+const word = "magnolia";
